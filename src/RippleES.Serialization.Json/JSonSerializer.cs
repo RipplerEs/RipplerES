c7fb890a0292efc2b46b03d28fea7ca7d1ec051e
RipplerES <richard.doucet@ripplerES.com>
2017-02-03 17:35:18 -0500

    Refactored / replaced serialization

---
+++ src/RippleES.Serialization.Json/JSonSerializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using RipplerES.CommandHandler;
+
+namespace RippleES.Serialization.Json
+{
+    public class JsonSerializer : ISerializer
+    {
+        public IAggregateEvent<T> Deserialize<T>(AggregateEventData aggregateEventData)
+        {
+            return (IAggregateEvent<T>)JsonConvert.DeserializeObject(aggregateEventData.Data, Type.GetType(aggregateEventData.Type));
+        }
+
+        public AggregateEventData Serialize<T>(IAggregateEvent<T> @event, Dictionary<string, string> metaData)
+        {
+            return new AggregateEventData
+            {
+                Data = JsonConvert.SerializeObject(@event),
+                MetaData = JsonConvert.SerializeObject(metaData),
+                Type = @event.GetType().AssemblyQualifiedName
+            };
+        }
+    }
+}
