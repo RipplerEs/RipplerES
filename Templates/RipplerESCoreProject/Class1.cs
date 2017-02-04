8d5f935f3e3da0daa48dc49a078fda6f16af94a9
Zebrai17 <rick.doucet@implementingeventsourcingwithfsharp.com>
2017-02-04 12:38:03 -0500

    Added Rippler Project Template and VSIX project to publish it up to the Visual Studio Gallery

---
+++ Templates/RipplerESCoreProject/Class1.cs
@@ -0,0 +1,38 @@
+﻿using RipplerES.CommandHandler;
+
+#region Aliases
+using Result = RipplerES.CommandHandler.IAggregateCommandResult<$safeprojectname$.Aggregate1>;
+#endregion
+
+namespace $safeprojectname$
+{
+    public class Aggregate1 : AggregateBase<Aggregate1>
+    {
+        private bool _someState = true;
+        public Result Execute(DoSomething command)
+        {
+            return !_someState 
+                        ? Error(new SomeError()) 
+                        : Success(new SomethingDone());
+        }
+
+        public void Apply(SomethingDone @event)
+        {
+            _someState = false;
+        }
+    }
+
+    public class DoSomething : IAggregateCommand<Aggregate1>
+    {
+        
+    }
+
+    public class SomethingDone : IAggregateEvent<Aggregate1>
+    {
+        
+    }
+
+    public class SomeError : IAggregateError<Aggregate1>
+    {
+    }
+}
